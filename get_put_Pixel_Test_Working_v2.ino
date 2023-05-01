c8479d2b7b26241a7e1f31476a07ef8548fafd8d
MikeyMoMo <15792417+MikeyMoMo@users.noreply.github.com>
2023-05-01 16:12:54 +0800

    Smooth scrolling, other fixes
    Smooth scrolling by using a tft_eSPI sprite.  24 frames per second with 30 stars.  14 frames/second using 200 stars. Other fixes for code beauty and minor stars fixes.

--- get_put_Pixel_Test_Working_v2.ino
+++ get_put_Pixel_Test_Working_v2.ino
@@ -4,8 +4,10 @@
 // Define TFT_eSPI object with the size of the screen:
 //  240 pixels width and 400 pixels height.
 //  We will rotate it later to portrait to get 400 width and 240 high.
-//#include "Setup23_TTGO_TM.h"  // Doesn't work when included heret
+//Use this: #include <User_Setups/Setup22_TTGO_T4_v1.3.h>
 TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite spriteHW = TFT_eSprite(&tft);
+
 // Setting PWM properties, do not change this!
 const int pwmFreq = 5000;
 const int pwmResolution = 8;
@@ -28,9 +30,9 @@ const int yCenter = TFT_HEIGHT / 2;
 #define YPOS 1
 #define DELTAY 2
 #define SCOLOR 3
-#define fallIterations 100
-#define numFlakes 25
-uint32_t icons[numFlakes][4];  // 0:XPOS, 1:YPOS, 2:Movement speed, 3:Star color
+#define fallIterations 1000
+#define numFlakes 30
+int32_t icons[numFlakes][4];  // 0:XPOS, 1:YPOS, 2:Movement speed, 3:Star color
 
 byte red = 31;
 byte green = 0;
@@ -47,9 +49,6 @@ bool oldSwapBytes;
 
 long int ap;
 unsigned long startMillis;
-
-int buffSize = (TFT_HEIGHT * TFT_WIDTH) / 2;
-uint16_t *workBuff;
 
 #define StarBitmapH 16
 #define StarBitmapW 16
@@ -87,51 +86,46 @@ void setup()
   ledcSetup(pwmLedChannelTFT, pwmFreq, pwmResolution);
   ledcWrite(pwmLedChannelTFT, ledBacklightFull);
   tft.invertDisplay(false); // Where it is true or false
-  tft.setRotation(1);
+  tft.setRotation(3);
 
-  workBuff = (uint16_t *)malloc(buffSize);
-  if (workBuff)
-    memset(workBuff, 0, buffSize);
-  else {
-    Serial.println("Buffer was not allocated.  Cannot continue."); while (1);
+  int *a;  // Sprite61 (the small one)
+  a = (int*)spriteHW.createSprite(TFT_WIDTH, TFT_HEIGHT);
+  if (a == 0) {
+    Serial.println("spriteHW creation failed.  Cannot continue.");
+    while (1);
   }
+  Serial.printf("createSprite61 dispWidth x dispHeight returned: %p\r\n", a);
 
   Serial.print("FreeHeapSize after allocating: ");
   Serial.println(xPortGetFreeHeapSize());
-  Serial.printf("Array located at %p.\n", workBuff);
+  Serial.printf("Array located at %p.\n", a);
 
   Serial.println("Creating Rainbow via call to rainbow_fill_array.");
-  rainbow_fill_array(workBuff);
-  Serial.println("Screen filled.");
-
-  Serial.println("Saving single color byte per row.");
-  for (currRow = 0; currRow < TFT_HEIGHT; currRow++)
-    bgSave[currRow] = tft.readPixel(0, currRow);
-  Serial.printf("Row colors saved, now rebuilding the Rainbow with drawFastHLine.\n");
+  rainbow_fill_array();
+  Serial.println("Background colors created.");
+  //  spritetest();  // Just to see how it is going so far.
+  //  delay(10000);  // Wait for it!
 }
 /***************************************************************************/
 void loop()
 /***************************************************************************/
 {
-  Serial.println("Clear the screen for fast draw with pushImage.");
-//  tft.fillScreen(0);
-  Serial.printf("The array starts at %p\n", workBuff);
-  Serial.println("Now, redraw the screen from the buffer with pushImage.");
-  oldSwapBytes = tft.getSwapBytes();
-  tft.setSwapBytes(!oldSwapBytes);
-  startMillis = millis();
-  tft.pushImage(0, 0, TFT_WIDTH, TFT_HEIGHT, workBuff);
-  Serial.printf("It took %i ms to draw rainbow with pushImage.\n", millis() - startMillis);
-  tft.setSwapBytes(oldSwapBytes);
+  for (currRow = 0; currRow < TFT_HEIGHT; currRow++)  // Put the candle back!
+    spriteHW.drawFastHLine(0, currRow, TFT_WIDTH, bgSave[currRow]);
 
-  Serial.println("Calling demoFallingStars routine.");
-  demoFallingStars(StarBitmap,  workBuff, fallIterations, numFlakes,
-                   StarBitmapW, StarBitmapH);
+  // With 25 flakes, getting 24 screen updates per second.
+  // With 50 flakes, getting 22 screen updates per second.
+  // With 200 flakes, down to 14 screen updates per second.
+
+  Serial.printf("Calling demoFallingStars with %i flakes & %i iterations.\r\n",
+                numFlakes, fallIterations);
+  demoFallingStars(StarBitmap, fallIterations, numFlakes, StarBitmapW, StarBitmapH);
   Serial.println("All done.");
+
   delay(5000);
 }
 /***************************************************************************/
-void demoFallingStars(const uint8_t *bitmap, uint16_t *ptr, int myIterations,
+void demoFallingStars(const uint8_t *bitmap, int myIterations,
                       int myFlakes, uint8_t w, uint8_t h)
 /***************************************************************************/
 {
@@ -154,82 +148,95 @@ void demoFallingStars(const uint8_t *bitmap, uint16_t *ptr, int myIterations,
   // Blue   0,     0, 255
   // Indigo 75,    0, 130
 
-  int currRow, b;
-  byte myByte;
-  int flake, myH, myW;
-  int byteWidth = (w + 7) / 8;
+  int flake, currRow;
+  //    int spriteReps = 0;  // Put this back in if you are tracking screen refresh rate.
 
   // Initialize all stars, assigning colors to each and draw all of them.
   for (flake = 0; flake < myFlakes; flake++) {
-    icons[flake][XPOS] = random(TFT_WIDTH);
-    icons[flake][YPOS] = random(TFT_HEIGHT * .8);;
-    icons[flake][DELTAY] = random(5) + 1;
+    icons[flake][XPOS]   = random(TFT_WIDTH);        // Random landscape positioning (X value)
+    icons[flake][YPOS]   = random(TFT_HEIGHT * .8);  // Start them all on-screen. (Y value)
+    icons[flake][DELTAY] = random(6) + 1;            // This is the fall speed. (Ensure != 0)
     icons[flake][SCOLOR] = GetStarColor();
   }
+  //  startMillis = millis();  // Put this back in if you are tracking screen refresh rate.
   while (myIterations-- > 0) {
-    for (flake = 0; flake < myFlakes; flake++) {
-      tft.drawBitmap(icons[flake][XPOS], icons[flake][YPOS], StarBitmap, StarBitmapW, StarBitmapH,
-                     icons[flake][SCOLOR]);
-    }
-    delay(100);
+
+    // Include this routine to see your frame refresh rate.
+    //        if ((millis() - startMillis) > 1000) {
+    //          Serial.printf("%i screen updates per second.\r\n", spriteReps);
+    //          startMillis = millis(); spriteReps = 0;
+    //        }
+
+    // First, put the requested number of flakes on the screen.
+    for (flake = 0; flake < myFlakes; flake++)
+      spriteHW.drawBitmap(icons[flake][XPOS], icons[flake][YPOS], StarBitmap, StarBitmapW, StarBitmapH,
+                          icons[flake][SCOLOR]);
+    // Then, update the screen.  The time to do this is just under 42 ms on my device.
+    //  No delay() needed.
+    spriteHW.pushSprite(0, 0);
+    //    delay(100);  // The pushSprite delay is sufficient.
+
+    // Get out on the last rep to leave the displayed stars still showing in their last positions.
     if (myIterations == 1) return;  // Leave the last update showing.
-    // Now erase the flakes by rewriting the background colors, proper rows,
+
+    // Now erase the flakes by rewriting the background colors, in their proper rows.
+    //  I only rewrite the back square that was "destroyed" by the star being written,
+    //  not the whole line.  That saves time and the full line write is not required.
+    //  It takes less than a ms to repair the "damage" but takes 34 ms to rewrite the entire
+    //  background with pushImage.  Just a few ms longer for a bunch of fill width drawFastHLine's.
+
     for (flake = 0; flake < myFlakes; flake++) {
       for (currRow = 0; currRow < 16; currRow++)
-        tft.drawFastHLine(icons[flake][XPOS], icons[flake][YPOS] + currRow, 16,
-                          bgSave[icons[flake][YPOS] + currRow]);
+        spriteHW.drawFastHLine(icons[flake][XPOS], icons[flake][YPOS] + currRow, 16,
+                               bgSave[icons[flake][YPOS] + currRow]);
       // Now adjust the position of each flake down the screen and re-init if off screen.
       if (icons[flake][YPOS] > tft.height()) {
         icons[flake][XPOS]   = random(tft.width());
-        icons[flake][YPOS]   = 0;
+        icons[flake][YPOS]   = -StarBitmapH;
         icons[flake][DELTAY] = random(5) + 1;
         icons[flake][SCOLOR] = GetStarColor();
       } else {  // Else, move it down the screen a bit.
         icons[flake][YPOS]  += icons[flake][DELTAY];
       }
     }
+    //        spriteReps++;  // Put this back in if you are tracking screen refresh rate.
   }
 }
 /***************************************************************************/
 uint32_t GetStarColor()
 /***************************************************************************/
 {
-  R = random(105) + 150;  // 2nd num is minimum brightness.  First is additive to that.
-  G = random(105) + 150;
-  B = random(105) + 150;
-  //  Serial.printf("R %i, G %i, B %i  or  %X\n", R, G, B, RGB888(R, G, B));
+  R = random(105) + 100;  // 2nd num is minimum brightness.  First is additive to that.
+  G = random(105) + 100;
+  B = random(105) + 100;
   return (RGB888(R, G, B));
 }
 // #########################################################################
 // Fill screen with a rainbow pattern and save each line color in an array.
 // #########################################################################
 /***************************************************************************/
-void rainbow_fill_array(uint16_t *ptr)
+void rainbow_fill_array()
 /***************************************************************************/
 {
+
+  // This routine came out of the tft_eSPI library from one of the sprite examples.
+
   // The colours and state are not reinitialised so the start colour
   //  changes each time the funtion is called.
-  uint16_t *my_ptr, debugCt;
-  my_ptr = workBuff;
   for (int row = 0; row < TFT_HEIGHT; row++) {
     for (int col = 0; col < TFT_WIDTH; col++) {
-      if (!my_ptr % 2) {
+      if (!col % 2) {
         colorbuild = colour;
         colorbuild = colorbuild << 16;
-        //        if (debugCt++ < 20) Serial.printf("Even. Colour = %x, colorbuild now: %X\n",
-        //                                            colour, colorbuild);
       } else {
         colorbuild = colorbuild | colour;
-        //        if (debugCt++ < 20) Serial.printf("Odd.  Colour = %x, colorbuild now: %X\n",
-        //                                            colour, colorbuild);
-        *my_ptr = colour;
       }
-      my_ptr++;
-      //      Serial.printf("%p\n", my_ptr);
     }
     // Draw a horizontal line 1 pixel high in the selected colour
     // Now propogate the color across the screen line with drawFastHLine.
-    tft.drawFastHLine(0, row, tft.width(), colour);
+    //    tft.drawFastHLine(0, row, tft.width(), colour);
+    // I am not doing this because it is no longer necessary.  It is being handled in loop().
+
     // This is a "state machine" that ramps up/down the colour brightnesses in sequence
     switch (state) {
       case 0:
@@ -276,5 +283,43 @@ void rainbow_fill_array(uint16_t *ptr)
         break;
     }
     colour = red << 11 | green << 5 | blue;
+    bgSave[row] = colour;  // This is where the background fixup color comes from.
   }
+}
+/***************************************************************************/
+void spritetest()  // Testing only.
+//                    Just used to be sure the sprite code was working.
+/***************************************************************************/
+{
+  spriteHW.fillSprite(TFT_YELLOW);
+  spriteHW.setCursor (12, 5);
+  spriteHW.print("Original ADAfruit font!");
+
+  spriteHW.setTextColor(TFT_RED, TFT_BLACK); // Do not plot the background colour
+
+  // Overlay the black text on top of the rainbow plot
+  //  (the advantage of not drawing the background colour!)
+  // Draw text centre at position 80, 12 using font 2
+  spriteHW.drawCentreString("Font size 2", 80, 14, 2);
+  // Draw text centre at position 80, 24 using font 4
+  spriteHW.drawCentreString("Font size 4", 80, 30, 4);
+  // Draw text centre at position 80, 24 using font 6
+  spriteHW.drawCentreString("12.34", 80, 54, 6);
+  // Draw text centre at position 80, 90 using font 2
+  spriteHW.drawCentreString("12.34 is in font size 6", 80, 92, 2);
+
+  // Note the x position is the top left of the font!
+
+  // draw a floating point number
+  float pi = 3.14159; // Value to print
+  int precision = 3;  // Number of digits after decimal point
+  int xpos = 50;      // x position
+  int ypos = 110;     // y position
+  // Font 6 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 0 : a p m
+  int font = 2;       // font number only 2,4,6,7 valid.
+  // Draw rounded number and return new xpos delta for next print position
+  xpos += spriteHW.drawFloat(pi, precision, xpos, ypos, font);
+  spriteHW.drawString(" is pi", xpos, ypos, font); // Continue printing from new x position
+
+  spriteHW.pushSprite(0, 0);
 }
